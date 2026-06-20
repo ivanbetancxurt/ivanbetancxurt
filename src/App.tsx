18932ae732dedf268290e0b969bf1234e60e2353
Ivan Betancourt <ivanab252@gmail.com>
2026-06-20 14:57:19 -0400

    Add links + temp README

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,18 @@ function App() {
         <>
             <div className='container'>
                 <div className='card'>
-                    <img src={ headshot } alt="Ivan's headshot" width='100px' />
-                    <div>
-                        list of links
-                    </div>
+                    <img src={ headshot } alt="Ivan's headshot" className='headshot' />
+                    <section className='links'>
+                        <div>
+                            <a href='https://github.com/ivanbetancxurt' target='_blank' rel='noopener noreferrer'>GitHub</a>                        
+                        </div>
+                        <div>
+                            <a href='https://www.linkedin.com/in/ivan-andre-betancourt/' target='_blank' rel='noopener noreferrer'>LinkedIn</a> 
+                        </div>
+                        <div>
+                            <a href='https://www.linkedin.com/in/ivan-andre-betancourt/' target='_blank' rel='noopener noreferrer'>sum else</a> 
+                        </div>
+                    </section>
                 </div>
             </div>
         </>
